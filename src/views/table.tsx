a6b703e655ea12a15a0a9a814372f03f5b451595
Richard HeRo <alexander.01093@gmail.com>
2023-07-20 01:17:12 -0500

    oderby

--- src/views/table.tsx
+++ src/views/table.tsx
@@ -3,10 +3,11 @@ import './../assets/css/table.css';
 import { TableData } from '../providers/tableData';
 
 function Table() {
-    const [dataOrigin, setDataOrigin]: any = useState();
-    const [dataOriginTwo, setDataOriginTwo]: any = useState();
+    const [dataOrigin, setDataOrigin]: any = useState([]);
+    const [dataOriginTwo, setDataOriginTwo]: any = useState([]);
     const [changeColor, setChangeColor]: any = useState(false);
     const [orderByCountry, setOrderByCountry]: any = useState(false);
+    const [workSearch, setWorkSearch]: any = useState('');
     const data = async () => {
         let getData: any = await TableData();
         if (getData) {
@@ -36,12 +37,12 @@ function Table() {
         setOrderByCountry(flag)
     }
     const deleteItem = (item: any) => {
-        let dataFilter : any = []
-        let dataFilterDeletes : any = []
-        dataOrigin.forEach((e:any) => {
-            if(e === item){
+        let dataFilter: any = []
+        let dataFilterDeletes: any = []
+        dataOrigin.forEach((e: any) => {
+            if (e === item) {
                 dataFilterDeletes.push(e)
-            }else{
+            } else {
                 dataFilter.push(e)
             }
         });
@@ -50,7 +51,42 @@ function Table() {
     const restoreData = (item: any) => {
         setDataOrigin(dataOriginTwo)
     }
-
+    const serachTable = (e: any) => {
+        let dataFilter : any = []
+        if(dataOriginTwo != undefined){
+            dataOriginTwo.forEach((data : any) => {
+                if(data.location.country.includes(e)){
+                    dataFilter.push(data);
+                }
+            });
+        }
+        setDataOrigin(dataFilter)
+    }
+    const orderBy = (e: any) => {
+        const dataC = JSON.stringify(dataOrigin);
+        const datConvert = JSON.parse(dataC)
+        switch (e) {
+            case '1':
+                datConvert.sort((a: any, b: any) => a.name.first.localeCompare(b.name.first))
+                break;
+            case '2':
+                datConvert.sort((a: any, b: any) => a.name.last.localeCompare(b.name.last))
+                break;
+            case '3':
+                datConvert.sort((a: any, b: any) => a.gender.localeCompare(b.gender))
+                break;
+            case '4':
+                datConvert.sort((a: any, b: any) => a.location.country.localeCompare(b.location.country))
+                break;
+        
+            default:
+                break;
+        }
+        setDataOrigin(datConvert)
+    }
+    useEffect(() => {
+        serachTable(workSearch)
+    }, [workSearch])
     return (
         <div>
             <h1 className='text-center text-[50px] mb-[50px]'>Lista de usuarios</h1>
@@ -63,7 +99,7 @@ function Table() {
                     <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                         <svg className="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"></svg>
                     </span>
-                    <input className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Buscar por País" type="text" name="search" />
+                    <input onChange={(e) => setWorkSearch(e.target.value)} value={workSearch} className="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Buscar por País" type="text" name="search" />
                 </label>
             </div>
             <div className='mr-auto ml-auto flex justify-center'>
@@ -71,10 +107,10 @@ function Table() {
                     <thead>
                         <tr>
                             <th className='w-28'>Foto</th>
-                            <th className='w-28'>Nombre</th>
-                            <th className='w-28'>Apellido</th>
-                            <th className='w-28'>Genero</th>
-                            <th className='w-28'>Pais</th>
+                            <th className='w-28 cursor-pointer' onClick={() => orderBy('1')} >Nombre</th>
+                            <th className='w-28 cursor-pointer' onClick={() => orderBy('2')}>Apellido</th>
+                            <th className='w-28 cursor-pointer' onClick={() => orderBy('3')}>Genero</th>
+                            <th className='w-28 cursor-pointer' onClick={() => orderBy('4')}>Pais</th>
                             <th className='w-28'>Accion</th>
                         </tr>
                     </thead>
